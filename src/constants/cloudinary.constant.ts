a18c5f0763f7335b3b30d734045e926b98e3dd3c
iqoypresto <rfqimaulana888@gmail.com>
2026-07-11 05:04:07 +0700

    Create constant for cloudinary folder (splitting code)

---
+++ src/constants/cloudinary.constant.ts
@@ -0,0 +1,6 @@
+export const CLOUDINARY_FOLDER = {
+    PAYMENT_PROOF: "arenahub/payment-proofs",
+    VENUE: "arenahub/venues",
+    COURT: "arenahub/courts",
+    PROFILE: "arenahub/profiles"
+} as const

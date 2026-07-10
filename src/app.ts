c804ee63818fbbda7dd706b8d54d17950cd153a3
iqoypresto <rfqimaulana888@gmail.com>
2026-07-11 05:32:53 +0700

    Add new line for dashboard route on main file

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { ErrorMiddleware } from "./middlewares/error.middleware";
-import { env } from "./config/env";
+import { env } from "./config/env.config";
 import cookieParser from "cookie-parser";
 import { AuthRoute } from "./features/auth/auth.route";
+import { CourtRoute } from "./features/court/court.route";
+import { BookingRoute } from "./features/booking/booking.route";
+import { VenueRoute } from "./features/venue/venue.route";
+import { BookingJob } from "./jobs/booking.job";
+import { DashboardRoute } from "./features/dashboard/dashboard.route";
 
 const app = express();
 
@@ -13,20 +18,26 @@ app.use(
     origin: env.WHITE_LIST,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    credentials: true,
   }),
 );
 app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(`${env.API_PREFIX}/auth`, AuthRoute)
+app.use(`${env.API_PREFIX}/auth`, AuthRoute);
+app.use(`${env.API_PREFIX}/venue`, VenueRoute);
+app.use(`${env.API_PREFIX}/court`, CourtRoute);
+app.use(`${env.API_PREFIX}/booking`, BookingRoute);
+app.use(`${env.API_PREFIX}/dashboard`, DashboardRoute)
 
 app.use(ErrorMiddleware);
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === "development") {
   app.listen(env.PORT, () => {
     console.log(`[⚡APP] Application is running on port: ${env.PORT}`);
-  })
+
+    BookingJob.start()
+  });
 }
-export default app
+export default app;

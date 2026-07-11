e3d3f09fbb4f66d4238fe427de5de736e8b13ab6
iqoypresto <rfqimaulana888@gmail.com>
2026-07-12 04:44:22 +0700

    Add logic for admin dashboard summary, waitingverification-list, and upcoming-booking

--- src/features/dashboard/dashboard.mapper.ts
+++ src/features/dashboard/dashboard.mapper.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@prisma/client";
+
 type DashboardSummaryResponse = {
   totalBookings: number;
   todayBookings: number;
@@ -8,8 +10,92 @@ type DashboardSummaryResponse = {
   todayRevenue: number;
 };
 
+type PendingVerficationBooking = Prisma.BookingGetPayload<{
+  select: {
+    id: true,
+    bookingCode: true,
+    startDatetime: true,
+    endDatetime: true,
+    totalPrice: true,
+    updatedAt: true,
+    player: {
+      select: {
+        id: true,
+        fullName: true
+      }
+    },
+    court: {
+      select: {
+        id: true,
+        name: true
+      }
+    }
+  }
+}>
+
+type UpcomingBooking = Prisma.BookingGetPayload<{
+  select: {
+    id: true,
+    bookingCode: true,
+    startDatetime: true,
+    endDatetime: true,
+    totalPrice: true,
+    player: {
+      select: {
+        id: true,
+        fullName: true
+      }
+    },
+    court: {
+      select: {
+        id: true,
+        name: true
+      }
+    }
+  }
+}>
+
 export class DashboardMapper {
   static toSummaryResponse(summary: DashboardSummaryResponse) {
     return summary;
+  }
+
+  static toPendingVerificationResponse(bookings: PendingVerficationBooking[]){
+    return bookings.map(booking => ({
+      id: booking.id,
+      bookingCode: booking.bookingCode,
+      player: {
+        id: booking.player.id,
+        fullName: booking.player.fullName
+      },
+      court: {
+        id: booking.court.id,
+        name: booking.court.name
+      },
+      date: booking.startDatetime.toISOString().split("T")[0],
+      startTime: booking.startDatetime.toISOString().split("T")[1]?.slice(0, 5),
+      endTime: booking.endDatetime.toISOString().split("T")[1]?.slice(0, 5),
+      totalPrice: Number(booking.totalPrice),
+      waitingSince: booking.updatedAt
+    }))
+  }
+
+  static toUpcomingBookingsResponse(bookings: UpcomingBooking[]){
+    return bookings.map(booking => ({
+      id: booking.id,
+      bookingCode: booking.bookingCode,
+      player: {
+        id: booking.player.id,
+        fullName: booking.player.fullName
+      },
+      court: {
+        id: booking.court.id,
+        name: booking.court.name
+      },
+      date: booking.startDatetime.toISOString().split("T")[0],
+      startTime: booking.startDatetime.toISOString().split("T")[1]?.slice(0, 5),
+      endTime: booking.endDatetime.toISOString().split("T")[1]?.slice(0, 5),
+      totalPrice: Number(booking.totalPrice)
+    }))
   }
 }
--- src/features/dashboard/dashboard.service.ts
+++ src/features/dashboard/dashboard.service.ts
@@ -4,14 +4,26 @@ import { ResponseError } from "../../utils/response-error.util";
 import { StatusCodes } from "http-status-codes";
 import { DashboardMapper } from "./dashboard.mapper";
 
-export class DashboardService{
-    static async summary(venueAdminId: string){
-        const venue = await DashboardRepository.findVenueByVenueAdminId(venueAdminId)
+export class DashboardService {
+  static async getDashboard(venueAdminId: string) {
+    const venue =
+      await DashboardRepository.findVenueByVenueAdminId(venueAdminId);
 
-        if(!venue) throw new ResponseError(StatusCodes.NOT_FOUND, "Venue not found.")
+    if (!venue)
+      throw new ResponseError(StatusCodes.NOT_FOUND, "Venue not found.");
 
-        const summary = await DashboardRepository.getSummary(venue.id)
-        
-        return DashboardMapper.toSummaryResponse(summary)
-    }
+    const [summary, pendingVerification, upcomingBookings] = await Promise.all([
+      DashboardRepository.getSummary(venue.id),
+      DashboardRepository.getPendingVerifications(venue.id),
+      DashboardRepository.getUpcomingBookings(venue.id, new Date()),
+    ]);
+
+    return {
+      summary: DashboardMapper.toSummaryResponse(summary),
+      pendingVerification:
+        DashboardMapper.toPendingVerificationResponse(pendingVerification),
+      upcomingBookings:
+        DashboardMapper.toUpcomingBookingsResponse(upcomingBookings),
+    };
+  }
 }
